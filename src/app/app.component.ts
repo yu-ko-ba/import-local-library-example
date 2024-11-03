5cf3e16196c70ccba7a47e0db81ad7e652849c3a
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2024-11-03 12:24:11 +0900

    install @yu-ko-ba/ngx-mat-little-diva-theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'import-local-library-example';
